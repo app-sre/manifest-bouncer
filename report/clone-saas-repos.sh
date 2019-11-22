e7814bb502f8c9a43b829dc0ec7e781758896bda
Jaime Melis <jmelis@redhat.com>
2019-11-22 08:34:47 +0100

    clone master instead of latest prod hash

--- report/clone-saas-repos.sh
+++ report/clone-saas-repos.sh
@@ -12,6 +12,11 @@ for saasrepo in `find "$WORKDIR" -mindepth 1 -maxdepth 1 -type d`; do
     cd "$saasrepo"
 
     for context in $(saasherder config get-contexts); do
+        for service in $(saasherder --environment production get-services --context $context); do
+            hash_len=$(saasherder --context $context --environment production get hash $service | wc -c)
+            [ "$hash_len" -eq "41" ] && saasherder --context $context --environment production update hash $service master
+        done
+
         saasherder --context $context --environment production pull
         saasherder --context $context --environment production template --local tag
     done
