b33f9b10068a975b2f26c2a134ab63f9d921db72
Maor Friedman <maor.friedman@redhat.com>
2019-12-02 16:47:19 +0200

    check all potential resources

---
+++ checks/public_resources.py
@@ -0,0 +1,30 @@
+from lib.base import CheckBase
+
+
+class CheckPublicResources(CheckBase):
+    whitelist = CheckBase.base_kinds['PodTemplateSpec']
+    whitelist.append('ConfigMap')
+
+    enable_parameter = 'public-resources'
+    description = 'check that all data in a resource is public'
+    default_enabled = False
+
+    def do_check(self, m, name):
+        forbidden_patterns = [
+            "https://",
+            ".svc.cluster.local"
+        ]
+        e_msg = f"Resource data should only be public: '{name}'"
+
+        if isinstance(m, str):
+            assert not any(fp in m for fp in forbidden_patterns), e_msg
+        elif isinstance(m, dict):
+            for v in m.values():
+                self.do_check(v, name)
+        elif isinstance(m, list):
+            for i in m:
+                self.do_check(i, name)
+
+    def check_public_resource(self, m):
+        name = m['metadata']['name']
+        self.do_check(m, name)
