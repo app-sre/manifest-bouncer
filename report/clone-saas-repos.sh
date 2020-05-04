95f6c6b9da3f9123fa2eec0aa352792ab940e830
Rob Rati <rrati@redhat.com>
2020-05-04 11:24:22 -0400

    Support providing a gitlab token for saasherder to use

--- report/clone-saas-repos.sh
+++ report/clone-saas-repos.sh
@@ -6,10 +6,19 @@ rm -rf "$WORKDIR"; mkdir -p "$WORKDIR"
 
 cd "$WORKDIR"
 
-cat - | xargs -P0 -n1 git clone -q
+REPOS=`cat -`
+echo "$REPOS" | xargs -P1 -n1 git clone -q
 
 for saasrepo in `find "$WORKDIR" -mindepth 1 -maxdepth 1 -type d`; do
     cd "$saasrepo"
+
+    project=`basename $saasrepo`
+    repo=`echo "$REPOS" | grep "$project"`
+    if grep -q "https://gitlab" <<< "$repo" && [[ ${#GITLAB_TOKEN} -gt 0 ]]; then
+        TOKEN="--token $GITLAB_TOKEN"
+    else
+        TOKEN=""
+    fi
 
     for context in $(saasherder config get-contexts); do
         for service in $(saasherder --environment production get-services --context $context); do
@@ -17,7 +26,7 @@ for saasrepo in `find "$WORKDIR" -mindepth 1 -maxdepth 1 -type d`; do
             [ "$hash_len" -eq "41" ] && saasherder --context $context --environment production update hash $service master
         done
 
-        saasherder --context $context --environment production pull
+        saasherder --context $context --environment production pull $TOKEN
         saasherder --context $context --environment production template --local tag
     done
 done
